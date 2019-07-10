11cf55cd65c278326ec2b5201ead4dccc2e7c35e
Jesper Hasselquist <psy08jh@gmail.com>
2019-07-10 22:19:24 +0200

    Remove old stuff

--- _posts/2017-3-18-First-post-HYPE.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title: First post! :D
----
-
-This is my first post.
-:call_me_hand:
--- js/timerthing.js
+++
@@ -1,7 +0,0 @@
-var t1 = new Date(2017, 3, 31, 19, 30, 0, 0);
-
-function timer() {
-    var dif = new Date().getTime() - t1.getTime();
-
-    return dif / 1000;
-}
