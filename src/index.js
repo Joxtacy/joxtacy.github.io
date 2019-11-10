64bd55bce270957dd0cdacbc53f7a5441e67cfe7
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2019-11-10 20:17:35 +0100

    Start fresh

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Welcome from "./js/components/presentational/Welcome.jsx";
-
-const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<Welcome />, wrapper) : false;
